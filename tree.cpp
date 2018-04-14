af409c51ae1c9d4308c6d5b7993ce59d07e05b90
DirtyDanTime <dgolob98@yahoo.com>
2018-04-14 15:37:20 -0500

    Added destructor
    added destructor and get val

--- tree.cpp
+++ tree.cpp
@@ -11,3 +11,31 @@ Node::Node()
 	leftChild->null;
 	rightChild->null;
 }
+
+Node::~Node()
+{
+	delete childOne;
+	delete childTwo;
+	delete childThree;
+	delete childFour;
+	delete childFive	
+}
+
+short Node::getVal(short loc)
+{
+	if(loc == 1)
+		return numOne;
+	else if(loc == 2)
+		return numTwo;
+	else if(loc == 3)
+		return numThree;
+	else if(loc == 4)
+		return numFour;
+	else
+		return;
+}
+
+void Node::setVal(short num)
+{
+	if()
+}
