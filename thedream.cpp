7f21eef2022ac21f3944ac852c868ef3c1ee39e7
DirtyDanTime <dgolob98@yahoo.com>
2018-04-16 00:35:05 -0500

    search..... again

--- thedream.cpp
+++ thedream.cpp
@@ -138,3 +138,41 @@ void merge(Node *node, short num)
 
 	return;
 }
+
+LeafNode * search(short num)
+{
+	LeafNode *temp = leaf();
+	temp = subSearch(root, NULL, num);
+	bool exist = false;
+
+	for(short i = 0; i < temp->stored; i++)
+	{
+		if(temp->values[i] == num)
+			exist = true;
+	}
+
+	if(exist == true)
+		return temp;
+	else 
+		return NULL;
+}
+
+LeafNode * subSearch(Node *node, Node *par, short num)
+{
+	if(node->isLeaf == true)
+	{
+		node->parent = par;
+		return leafSwap(node);
+	}
+
+	
+	if(num < node->values[0])
+		return subSearch(node->children[0], node, num);
+	else if(node->values[0] < num && num < node->values[1])
+		return subSearch(node->children[1], node, num);
+	else if(node->values[1] < num && num < node->values[2])
+		return subSearch(node->children[2], node, num);
+	else if(node->values[2] < num)
+		return subSearch(node->children[3], node, num);
+
+}
