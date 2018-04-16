29b777bf603603beeaa49cdd863c296e11d9cf92
DirtyDanTime <dgolob98@yahoo.com>
2018-04-16 00:14:31 -0500

    Search again
    added search again

--- thedream.cpp
+++ thedream.cpp
@@ -97,3 +97,41 @@ void merge(Node *node, short num)
 
 	return;
 }
+
+LeafNode * search(short num)
+{
+	LeafNode *temp = leaf();
+	temp = subSearch(root, NULL, num)
+	bool exist = false;
+
+	for(short i = 0; i < temp->stored; i++)
+	{
+		if(temp->values[i] == num)
+			exist = true;
+	}
+
+	if(exist == true)
+		return temp;
+	else 
+		return NULL;
+}
+
+LeafNode * subSearch(Node *node, Node *par, short num)
+{
+	if(node->isLeaf == true)
+	{
+		node->parent = par;
+		return node;
+	}
+
+	
+	if(num < node->values[0])
+		return search(node->children[0], node, num);
+	else if(node->values[0] < num && num < node->values[1])
+		return search(node->children[1], node, num);
+	else if(node->values[1] < num && num < node->values[2])
+		return search(node->children[2], node, num);
+	else if(node->values[2] < num)
+		return search(node->children[3], node, num);
+
+}
